4b09defb31a6355f8a67890a3add8760d515d5bb
wangkaiwd <1162486176@qq.com>
2023-04-24 01:30:26 +0800

    feat: add annotation for code

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
+  // handle incoming requests and returning response to the client
   controllers: [AppController],
   providers: [AppService],
 })
