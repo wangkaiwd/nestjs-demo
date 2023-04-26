427ba559fc1009b14e926785f1614a183c721cf5
wangkaiwd <1162486176@qq.com>
2023-04-26 20:33:22 +0800

    feat: support import json file

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,12 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { SWAGGER_PATH } from './constants/config';
+import pkg from '../package.json';
 
+console.log('pkg', pkg);
 export const generateSwaggerDocument = (app: INestApplication) => {
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
+    .setTitle(pkg.name)
+    .setDescription(pkg.description)
+    .setVersion(pkg.version)
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
